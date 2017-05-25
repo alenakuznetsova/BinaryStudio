ec36e3d7b983d6b931de9f216dc1cad0e48dc469
alena kuznetsova <alena.klevtsova@gmail.com>
2017-05-25 23:32:50 +0300

    #8: add task 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # BinaryStudio
 tasks from AcademyBinaryStudio
 
-[BSA 17 JS. Lecture 1 - Git](https://github.com/alenakuznetsova/BinaryStudio/tree/master/1-week)
+[BSA 17 JS. Lecture 1 - Git](https://github.com/alenakuznetsova/BinaryStudio/tree/master/task1)
+[BSA 17 JS. Lecture 2 - JS](https://github.com/alenakuznetsova/BinaryStudio/tree/master/task2)
+
---
+++ task2/task2.js
@@ -0,0 +1,51 @@
+class Fighter{ 
+  constructor(name, power=50, health=100) {
+    this.name = name;
+    this.power = power;
+    this.health = health;
+  }
+  
+  setDamage(damage){
+    this.health = this.health - damage;
+    console.log('health:' + this.health);
+  } 
+  
+  hit(enemy, point=10) {
+    enemy.setDamage(point * this.power); 
+  }
+  
+  toString(){
+    return `${this.name} [ his health: ${this.health}, his power: ${this.power} ]`;
+  }
+}
+
+class ImprovedFighter extends Fighter {  
+  hit(enemy,point) {
+    super.hit(enemy, point * 2);
+  }
+}
+
+let fight = (fighter1, fighter2, ...points) => {
+  
+  let fighters = [fighter1, fighter2];
+
+  for(let point of points) {
+       
+       fighters[0].hit(fighters[1],point);
+    
+       if(fighters[1].health < 1) {
+          return fighters[0];
+       } 
+        
+        fighters.reverse();
+  }
+  
+  return `not ${fighter1} or ${fighter2}`;
+}
+
+let fighter = new Fighter('Tom', 4, 75);
+let improvedFighter = new ImprovedFighter('Jack', 2, 150);
+
+let result = `Winner is ${fight(fighter, improvedFighter,...[10])}`;
+
+console.log(result);
